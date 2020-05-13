7950b74e48617fe4e80a3ba28803ed144846edb3
Matheus Henrique <matheussilva770@gmail.com>
2020-05-13 20:41:03 -0300

    first commit- add sbadmin2, create user admin and dashboard

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,30 @@
+class ApplicationController < ActionController::Base
+
+  layout :layout_by_resource
+
+  rescue_from ActiveRecord::DeleteRestrictionError, with: :impossible_to_delete
+  
+  private
+  
+  def layout_by_resource
+    if devise_controller?
+      "devise_layout"
+    else
+      "application"
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    if resource_name == :user 
+      admin_root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope) 
+    if resource_or_scope == :user 
+      new_user_session_path
+    #elsif resource_or_scope == :company_user
+    #  new_company_user_session_path
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users, path: 'admin/users'
+  namespace :admin do
+    get 'dashboard/index'
+    root 'dashboard#index'
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
