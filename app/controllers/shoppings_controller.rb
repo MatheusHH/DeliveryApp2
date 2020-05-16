bed0a0735758a0cc918cca4eb1facbb7f511cab6
Matheus Henrique <matheussilva770@gmail.com>
2020-05-15 23:38:47 -0300

    add shopping page with products and categories

---
+++ app/controllers/shoppings_controller.rb
@@ -0,0 +1,14 @@
+class ShoppingsController < ApplicationController
+  layout 'customer_layout'
+  before_action :load_categories
+
+  def index
+    @products = Product.all
+  end
+
+  private
+
+   def load_categories
+     @categories = Category.order(:name)
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     resources :categories
     resources :products
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'shoppings/index'
+  root 'shoppings#index'
 end
