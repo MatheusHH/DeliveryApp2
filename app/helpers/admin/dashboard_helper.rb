fc9357597d1efad154959247aab7c03963f10d31
Matheus Henrique <matheussilva770@gmail.com>
2020-06-22 00:21:20 -0300

    add average to admin dashboard and change id from show product because it was wrong

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -1,2 +1,9 @@
 module Admin::DashboardHelper
+  def average_ticket(value)
+    average = value / 30
+    average = average * 1.00
+    average = average / 100
+    average = number_to_currency(average, precision: 2, unit: "", separator: ",", delimiter: ".", format: "%n %u")
+    average 
+  end
 end
