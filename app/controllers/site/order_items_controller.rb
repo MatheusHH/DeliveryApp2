cee8acc6202aa2a63c3452ad29ceab59d7598fab
Matheus Henrique <matheussilva770@gmail.com>
2020-08-14 22:42:29 -0300

    Atualiza item ja adicionado

--- app/controllers/site/order_items_controller.rb
+++ app/controllers/site/order_items_controller.rb
@@ -1,22 +1,22 @@
 class Site::OrderItemsController < SiteController
+  before_action :set_current_order
 
-  def create
-    @order = current_order
-    @order_item = @order.order_items.new(order_item_params)
+  def create 
+    @order_item = @order.order_items.find_by(product_id: params[:order_item][:product_id])
+    @order_item ||= @order.order_items.new(order_item_params)
+    @order_item.update(order_item_params)
     @order.save
     session[:order_id] = @order.id
   end
 
-  def update
-    @order = current_order
+  def update  
     @order_item = @order.order_items.find(params[:id])
-    @order_item.update_attributes(order_item_params)
+    @order_item.update(order_item_params)
     @order.save
     @order_items = current_order.order_items
   end
 
-  def destroy
-    @order = current_order
+  def destroy  
     @order_item = @order.order_items.find(params[:id])
     @order_item.destroy
     @order.save
@@ -26,8 +26,11 @@ class Site::OrderItemsController < SiteController
 
   private
    
-    def order_item_params
-      params.require(:order_item).permit(:product_id, :quantity)
-    end
-  
+  def order_item_params
+    params.require(:order_item).permit(:product_id, :quantity)
+  end
+
+  def set_current_order
+    @order = current_order
+  end
 end
